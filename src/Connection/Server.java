766f2f352c9ab1b22473b0bf18af1a34a85d8e50
Bubble Inc <bbles.tech@gmail.com>
2016-10-19 11:57:54 +0200

    Fill the run method in both client and server side

--- src/Connection/Server.java
+++ src/Connection/Server.java
@@ -1,20 +1,23 @@
 package Connection;
 
+import Security.Certificat;
+import Security.PaireClesRSA;
+
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.HashMap;
 
 /**
  * Created by marouanebenalla on 07/10/2016.
  */
 public class Server extends IOOperation {
-    private boolean isServerMode = false;
-    protected short port = 12345;
-    protected ServerSocket server = null;
+    protected int port = 12345;
 
-    public Server(short port) {
+
+    public Server(int port) {
         try {
             server = new ServerSocket(port);
+            maCle = new PaireClesRSA(1024);
 
         } catch (IOException e) {
             System.out.println("Error 1" + e.getMessage());
@@ -25,13 +28,29 @@ public class Server extends IOOperation {
         try {
             while (true) {
                 socket = server.accept();
-                System.out.println(read());
-                write("Thank you for your solution");
-                System.out.println(read());
-                write("Thank you for your solution");
+                SocketBody request = read();
+                switch (request.getOption()) {
+                    case 1:
+                        //
+                        print("Do you accept the connection of the user ?");
+                        print("Connection...");
+                        SocketBody response = new SocketBody();
+                        connect(request.getBody(), response);
+                        print("Connection accepter!");
+                        write(response);
+                        break;
+                    case 2:
+                        print("Get the component CA");
+                        break;
+                    case 3:
+                        print("Get the componenet DA");
+                        break;
+                    default:
+                        print("Unknow option");
+                        socket.close();
+                }
                 socket.close();
             }
-
         } catch (IOException e) {
             System.out.println("Error 2" + e.getMessage());
 
@@ -40,10 +59,15 @@ public class Server extends IOOperation {
         }
     }
 
+    public boolean connect(HashMap<String, String> body, SocketBody response) {
+        response.setSuccess();
+        return true;
+    }
+
+    public boolean acceptConnection()
+
     public static void main(String[] args) {
         Server server = new Server(3000);
-        //while (true) {
         server.run();
-        //}
     }
 }
--- src/Console/JSONParser.java
+++ src/Console/JSONParser.java
@@ -1,12 +1,15 @@
 package Console;
 
 import Component.Equipement;
+import Connection.SocketBody;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
 
 /**
  * Created by marouanebenalla on 12/10/2016.
@@ -19,5 +22,15 @@ public class JSONParser {
         JsonReader json_file = new JsonReader(new FileReader(COMP_DIRECTORY + compName + ".json"));
         return new Gson().fromJson(json_file, Equipement.class);
 
+    }
+
+    // Serialize the object to be send to the prod server
+    public static String serialize(SocketBody response) {
+        return new Gson().toJson(response);
+    }
+
+    // Deserializer the object
+    public static SocketBody deserialize(String request) {
+        return new Gson().fromJson(request, SocketBody.class);
     }
 }
