24bfe86047186a9b2c9250832cd4b5c4d84f1d51
Marouane BENALLA <marouane.benalla@supelec.fr>
2016-10-23 01:08:27 +0200

    update the Certificat class

--- src/Security/Certificat.java
+++ src/Security/Certificat.java
@@ -1,6 +1,7 @@
 package Security;
 
 import java.math.BigInteger;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 
 import org.bouncycastle.asn1.x500.X500Name;
@@ -41,6 +42,27 @@ public class Certificat {
         }
     }
 
+    public Certificat(StringBuffer Name, PublicKey publicKey, PrivateKey privateKey, int validityDays){
+        //Define the SubjectPubicKeyInfo
+        SubjectPublicKeyInfo subPubKeyInfo = SubjectPublicKeyInfo.getInstance(key.pubKey().getEncoded());
+        Date startDate = new Date(System.currentTimeMillis());
+        Date endDate = new Date(System.currentTimeMillis() + validityDays * 24 * 60 * 60 * 1000);
+        X509v3CertificateBuilder CertGen = new X509v3CertificateBuilder(
+                new X500Name("CN=" + name),
+                BigInteger.ONE,
+                startDate,
+                endDate,
+                new X500Name("CN=" + name),//We set the subject of the certfifcat
+                subPubKeyInfo
+        );
+        try {
+            ContentSigner sigGen = new JcaContentSignerBuilder("SHA1withRSA").setProvider("BC").build(key.privKey());
+            x509 = CertGen.build(sigGen);
+        } catch (OperatorCreationException e) {
+
+        }
+    }
+
     public boolean verifiCerif(PublicKey publicKey) {
         // return True, if the certificat is valid
         return x509.isValidOn(new Date());
@@ -50,7 +72,8 @@ public class Certificat {
     public PublicKey getPublicKey() {
         return (PublicKey) x509.getSubjectPublicKeyInfo().getPublicKeyData();
     }
-    public static serialize(){
+
+    public static serialize() {
 
     }
 }
