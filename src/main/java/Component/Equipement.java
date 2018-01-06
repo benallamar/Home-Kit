66664436c67d272b001dc6bfdbf630955cb23ebc
Marouane Benalla <m.benalla@criteo.com>
2018-01-06 23:51:57 +0100

    Add bouncyCastle dependency to the code
    I added some TODO tags in order to enhance the code in the future

--- src/main/java/Component/Equipement.java
+++ src/main/java/Component/Equipement.java
@@ -3,9 +3,6 @@ package Component;
 import Connection.Server;
 import HomeSecurityLayer.Certificat;
 import Interfaces.IHMEquipement;
-import org.bouncycastle.crypto.params.RSAKeyParameters;
-import org.bouncycastle.crypto.util.PublicKeyFactory;
-
 import java.io.IOException;
 import java.security.PublicKey;
 import java.util.HashMap;
@@ -15,124 +12,126 @@ import java.util.HashSet;
  * Project Name : TL_crypto
  */
 public class Equipement extends Server {
-    private IHMEquipement display = null;
 
-    public Equipement(String name, int port) {
-        // Define the component
-        super(name, port);
+  private IHMEquipement display = null;
+
+  public Equipement(String name, int port) {
+    // Define the component
+    super(name, port);
+  }
+
+  public boolean isEqual(String name) {
+    return this.name == name;
+  }
+
+  public boolean isEqual(Equipement equipement) {
+    return name() == equipement.name();
+  }
+
+
+  public String affichage() {
+    String message = "Component: " + name;
+    message += "\nPort:      " + port;
+    message += "\nPublic Key:      " + maCle.pubKey().toString().substring(0, 30) + "...";
+    return message;
+  }
+
+  public String affichageCA(int port) {
+    Object[] ca = CA.get(port);
+    return ca.toString();
+  }
+
+  public String name() {
+
+    return "Equi: " + name.toUpperCase() + "\n Port: " + port;
+  }
+
+  public PublicKey maClePub() {
+    // Return the publicKe
+    return monCert.getPublicKey();
+  }
+
+  public Equipement setCertificateForChild(Equipement childComponent) {
+    return this;
+  }
+
+  public Equipement addChildComp(Equipement childComponent) {
+    return this;
+  }
+
+
+  public Equipement setMonCert(Certificat cert) {
+    monCert = cert;
+    return this;
+  }
+
+  public HashSet<Equipement> getChilds() {
+    return new HashSet<Equipement>();
+  }
+
+  public Certificat maCertif() {
+    return monCert;
+  }
+
+  public String toString() {
+    String description = "";
+    description += "";
+    return description;
+  }
+
+  public void run(HashSet<Equipement> systemComponent) {
+    //Here We have to establish the communication with the other component
+
+    super.run();
+
+  }
+
+  public boolean connectedWith(Equipement equi) {
+    return CA.containsKey(equi.port);
+  }
+
+  public void update() {
+    super.update();
+    if (display != null) {
+      display.repaint();
     }
+  }
 
-    public boolean isEqual(String name) {
-        return this.name == name;
+  public HashMap<Integer, Object[]> getCA() {
+    return CA;
+  }
+
+  public void display() {
+    display = new IHMEquipement(this);
+  }
+
+  public String[] CAArray() {
+    String[] array = new String[CA.size()];
+    int i = 0;
+    for (Object[] obj : CA.values()) {
+      array[i] = ((Certificat) obj[1]).getIssuer();
+      i++;
     }
+    return array;
+  }
 
-    public boolean isEqual(Equipement equipement) {
-        return name() == equipement.name();
+  public String getCertCA(int index) throws IOException {
+    int i = 0;
+    for (Object[] obj : CA.values()) {
+      if (index == i) {
+        return ((Certificat) obj[1]).display();
+      }
+      i++;
     }
+    return "No certificate with the given index";
+  }
 
-
-    public String affichage() {
-        String message = "Component: " + name;
-        message += "\nPort:      " + port;
-        message += "\nPublic Key:      " + maCle.pubKey().toString().substring(0, 30) + "...";
-        return message;
+  public static boolean equiExist(String name, int port, HashSet<Equipement> equipements) {
+    for (Equipement equipement : equipements) {
+      if (equipement.port == port) {
+        return true;
+      }
     }
-
-    public String affichageCA(int port) {
-        Object[] ca = CA.get(port);
-        return ca.toString();
-    }
-
-    public String name() {
-
-        return "Equi: " + name.toUpperCase() + "\n Port: " + port;
-    }
-
-    public PublicKey maClePub() {
-        // Return the publicKe
-        return monCert.getPublicKey();
-    }
-
-    public Equipement setCertificateForChild(Equipement childComponent) {
-        return this;
-    }
-
-    public Equipement addChildComp(Equipement childComponent) {
-        return this;
-    }
-
-
-    public Equipement setMonCert(Certificat cert) {
-        monCert = cert;
-        return this;
-    }
-
-    public HashSet<Equipement> getChilds() {
-        return new HashSet<Equipement>();
-    }
-
-    public Certificat maCertif() {
-        return monCert;
-    }
-
-    public String toString() {
-        String description = "";
-        description += "";
-        return description;
-    }
-
-    public void run(HashSet<Equipement> systemComponent) {
-        //Here We have to establish the communication with the other component
-
-        super.run();
-
-    }
-
-    public boolean connectedWith(Equipement equi) {
-        return CA.containsKey(equi.port);
-    }
-
-    public void update() {
-        super.update();
-        if (display != null)
-            display.repaint();
-    }
-
-    public HashMap<Integer, Object[]> getCA() {
-        return CA;
-    }
-
-    public void display() {
-        display = new IHMEquipement(this);
-    }
-
-    public String[] CAArray() {
-        String[] array = new String[CA.size()];
-        int i = 0;
-        for (Object[] obj : CA.values()) {
-            array[i] = ((Certificat) obj[1]).getIssuer();
-            i++;
-        }
-        return array;
-    }
-
-    public String getCertCA(int index) throws IOException {
-        int i = 0;
-        for (Object[] obj : CA.values()) {
-            if (index == i) {
-                return ((Certificat) obj[1]).display();
-            }
-            i++;
-        }
-        return "No certificate with the given index";
-    }
-
-    public static boolean equiExist(String name, int port, HashSet<Equipement> equipements) {
-        for (Equipement equipement : equipements) {
-            if (equipement.port == port) {
-                return true;
-            }
-        }
-        return false;
-    }
+    return false;
+  }
 }
--- src/main/java/HomKit/Home.java
+++ src/main/java/HomKit/Home.java
@@ -1,84 +1,80 @@
 package HomKit;
 
 import Component.Equipement;
-import Console.FilerParser;
 import Console.JSONParser;
 import Interfaces.IHMHome.IHMHome;
 import Interfaces.IHMHome.Loading;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Project Name : TL_crypto
  */
 public class Home extends Thread implements Runnable {
-    private HashSet<Equipement> equipements = new HashSet<Equipement>();
-    private static IHMHome homeInterface = null;
-    private boolean check_new_equi = false;
-    final static int TIME_CHECK = 5000; //Check the new equipements every TIME_CHECK Seconds
-    public static Loading loadPage = null;
-    public static boolean newCo = false;
 
-    public void checkComponent() {
-        try {
-            List<File> filesInFolder = Files.walk(Paths.get("/Users/marouanebenalla/Documents/workspace/sis/TL_Crypto/src/data/CompData/"))
-                    .filter(Files::isRegularFile)
-                    .map(Path::toFile)
-                    .collect(Collectors.toList());
-            int size = filesInFolder.size();
-            for (File file : filesInFolder) {
-                //We check if we don't already have this equipements
-                JSONParser.genrateEquipement(file.getName(), equipements);
-            }
-            loadPage.setValue(20);
-        } catch (IOException e) {
-            System.out.print(e.fillInStackTrace());
+  private HashSet<Equipement> equipements = new HashSet<Equipement>();
+  private static IHMHome homeInterface = null;
+  private boolean check_new_equi = false;
+  final static int TIME_CHECK = 5000; //Check the new equipements every TIME_CHECK Seconds
+  public static Loading loadPage = null;
+  public static boolean newCo = false;
+
+  public void checkComponent() {
+    try {
+      //TODO: Update the code of this page
+      List<File> filesInFolder = Files.walk(Paths.get("location_path"))
+          .filter(Files::isRegularFile)
+          .map(Path::toFile)
+          .collect(Collectors.toList());
+      int size = filesInFolder.size();
+      for (File file : filesInFolder) {
+        //We check if we don't already have this equipements
+        JSONParser.genrateEquipement(file.getName(), equipements);
+      }
+      loadPage.setValue(20);
+    } catch (IOException e) {
+      System.out.print(e.fillInStackTrace());
+    }
+  }
+
+  //Check for new componenet
+  public void checkNewComponent() {
+    while (true) {
+      try {
+        checkComponent();
+        Thread.sleep(TIME_CHECK);
+        if (newCo) {
+          homeInterface.update();
         }
+      } catch (InterruptedException e) {
+
+      }
+    }
+  }
+
+  public void run() {
+
+    if (!check_new_equi) {
+      //If is the first time we reolad the equipement
+      loadPage = new Loading();
+      checkComponent();
+      homeInterface = new IHMHome(equipements);
+      homeInterface.update();
+      check_new_equi = true;
+      new Thread(this).start();
+    } else {
+      checkNewComponent();
     }
 
-    //Check for new componenet
-    public void checkNewComponent() {
-        while (true) {
-            try {
-                checkComponent();
-                Thread.sleep(TIME_CHECK);
-                if (newCo) {
-                    homeInterface.update();
-                }
-            } catch (InterruptedException e) {
+  }
 
-            }
-        }
-    }
-
-    public void run() {
-
-        if (!check_new_equi) {
-            //If is the first time we reolad the equipement
-            loadPage = new Loading();
-            checkComponent();
-            homeInterface = new IHMHome(equipements);
-            homeInterface.update();
-            check_new_equi = true;
-            new Thread(this).start();
-        } else {
-            checkNewComponent();
-        }
-
-    }
-
-    public static void update() {
-        homeInterface.update();
-    }
+  public static void update() {
+    homeInterface.update();
+  }
 }
